476e7092e28f9a01e3d00e3ab458cf9fa4df988b
Jan Polivka <j.polivka5@gmail.com>
2023-04-21 19:50:18 +0000

    working

--- backend/src/loader/utility.ts
+++ backend/src/loader/utility.ts
@@ -1,6 +1,8 @@
 export const getTodayDDMMYY = (): string => {
     const epoch = Date.now()
     console.log(epoch)
+    const dateObj = new Date(epoch)
+    console.log(dateObj)
     // 
     return epoch.toString()
 }
