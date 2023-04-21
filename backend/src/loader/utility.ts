b745d5c46bf41f5641b134228d982cd399b6c79a
Jan Polivka <j.polivka5@gmail.com>
2023-04-21 20:01:41 +0000

    working

--- backend/src/loader/utility.ts
+++ backend/src/loader/utility.ts
@@ -2,7 +2,6 @@ export const getTodayDDMMYY = (): string => {
     const epoch = Date.now()
     console.log(epoch)
     const dateObj = new Date()
-    console.log(dateObj.toLocaleString())
     console.log(dateObj.getDate())
     console.log(dateObj.getMonth())
     console.log(dateObj.getFullYear())
