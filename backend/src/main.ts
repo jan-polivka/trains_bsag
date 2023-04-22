936199afbdbd6d24f58a713e6fd0ee63baca8e9a
Jan Polivka <j.polivka5@gmail.com>
2023-04-22 20:44:55 +0000

    working

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,7 @@ import { connectionGet, stationGet } from './loader/loader'
 import * as fs from 'fs'
 import * as fileHandle from 'fs/promises'
 import { loadConfig, sendMail } from './mailer/mailer'
+import { isConnectionCancelled, isConnectionDelayed } from './connection_processor/connection_processor'
 
 const fastify = Fastify({ logger: true })
 
@@ -36,6 +37,8 @@ fastify.get('/connection', async (req, res) => {
 fastify.get('/mail', async (req, res) => {
     let config = await loadConfig('config_default.yaml')
     // how do we know, that a train is happening?
+    // const connection = await connectionGet()
+    // const isConnectionOK = isConnectionCancelled(connection) && isConnectionDelayed(connection)
     let mailRes = await sendMail(config, false)
     res.send("go away")
 })
