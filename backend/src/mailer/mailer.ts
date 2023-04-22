87f963eaa0edb3f21888904cf87a1f7fcd842152
Jan Polivka <j.polivka5@gmail.com>
2023-04-22 11:49:15 +0000

    working

--- backend/src/mailer/mailer.ts
+++ backend/src/mailer/mailer.ts
@@ -26,5 +26,5 @@ export const loadConfig = async (filename: string) => {
     const file = await fs.promises.readFile(path)
     console.log(file.toString())
     let res = yaml.load(file.toString())
-    console.log(res["auth"])
+    console.log(res)
 }
