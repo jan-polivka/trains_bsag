8897f052d116dddd455b41af2ac6e45a8c5809a0
Jan Polivka <j.polivka5@gmail.com>
2023-04-22 20:28:48 +0000

    working

--- backend/src/connection_processor/connection_processor.test.ts
+++ backend/src/connection_processor/connection_processor.test.ts
@@ -10,6 +10,13 @@ test("connection is not cancelled", () => {
     assert.equal(result, false)
 })
 
+
+test("connection is cancelled", () => {
+    const connection: Connection = { id: "0", departure: { canceled: "1", delay: "0", time: "123" } }
+    const result = isConnectionCancelled(connection)
+    assert.equal(result, true)
+})
+
 test("extract the zeroth connection", () => {
     const path = `${cwd()}/resources/test/connection.json`
     const file = fs.readFileSync(path)
