562875fc0501c8e08a8435f1776479fc3334fad6
Jan Polivka <j.polivka5@gmail.com>
2023-04-22 08:52:42 +0000

    working

--- backend/src/mailer/mailer.ts
+++ backend/src/mailer/mailer.ts
@@ -15,4 +15,5 @@ export const loadConfig = async (filename: string) => {
     const path = `${cwd()}/config/${filename}`
     console.log(path)
     const file = await fs.promises.readFile(path)
+    console.log(file)
 }
