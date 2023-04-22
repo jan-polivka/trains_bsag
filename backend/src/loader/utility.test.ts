f65869a75be49aea43964d2bf9f8166cf8d892c4
Jan Polivka <j.polivka5@gmail.com>
2023-04-22 06:44:00 +0000

    working

--- backend/src/loader/utility.test.ts
+++ backend/src/loader/utility.test.ts
@@ -3,6 +3,8 @@ import { test } from "node:test";
 import { getTodayDDMMYY } from "./utility";
 
 test("getTodayDDMMYY returns today's date as DDMMYY", () => {
+    const date = new Date()
+    console.log(date)
     const res = getTodayDDMMYY(new Date())
     ///
     assert.strictEqual(res, "220423")
