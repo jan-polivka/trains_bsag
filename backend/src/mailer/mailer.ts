fd38690c16b4cf40aa2ef0309f034902b712fd19
Jan Polivka <j.polivka5@gmail.com>
2023-04-22 11:50:40 +0000

    working

--- backend/src/mailer/mailer.ts
+++ backend/src/mailer/mailer.ts
@@ -12,11 +12,11 @@ class MailerConfig {
     }
 
     constructor(parsedYaml: any) {
-        this.host = parsedYaml["host"] ? parsedYaml["host"] : null
-        this.port = parsedYaml["port"] ? parsedYaml["port"] : null
-        this.secure = parsedYaml["secure"] ? parsedYaml["secure"] : null
-        this.auth.user = parsedYaml["auth"]["user"] ? parsedYaml["auth"]["user"] : null
-        this.auth.pass = parsedYaml["auth"]["pass"] ? parsedYaml["auth"]["pass"] : null
+        // this.host = parsedYaml["host"] ? parsedYaml["host"] : null
+        // this.port = parsedYaml["port"] ? parsedYaml["port"] : null
+        // this.secure = parsedYaml["secure"] ? parsedYaml["secure"] : null
+        // this.auth.user = parsedYaml["auth"]["user"] ? parsedYaml["auth"]["user"] : null
+        // this.auth.pass = parsedYaml["auth"]["pass"] ? parsedYaml["auth"]["pass"] : null
     }
 }
 
