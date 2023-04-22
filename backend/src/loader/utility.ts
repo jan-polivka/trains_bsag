a23b2155ab6047b1f52a13ddf6a4bdc979352d71
Jan Polivka <j.polivka5@gmail.com>
2023-04-22 06:41:20 +0000

    working

--- backend/src/loader/utility.ts
+++ backend/src/loader/utility.ts
@@ -1,5 +1,6 @@
 export const getTodayDDMMYY = (): string => {
     const dateObj = new Date()
+    console.log(dateObj)
     const dateMonth = (dateObj.getMonth() + 1).toString()
     const month = parseInt(dateMonth) < 10 ? "0" + dateMonth : dateMonth
     const year = dateObj.getFullYear().toString().slice(-2)
