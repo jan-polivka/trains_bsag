8b277f9a9488c8127bc3c1b1436f5d109feb18ab
Jan Polivka <j.polivka5@gmail.com>
2023-04-23 17:03:48 +0000

    working

--- frontend/__tests__/SubscribeForm.test.tsx
+++ frontend/__tests__/SubscribeForm.test.tsx
@@ -8,7 +8,7 @@ describe('SubscribeForm', () => {
     it('irenders the subscribe form', async () => {
         const user = userEvent.setup()
         const mockSubmitTimeString = jest.fn()
-        render(<SubscribeForm submitTimeString={submitTimeString} />)
+        render(<SubscribeForm submitTimeString={mockSubmitTimeString} />)
         await user.type(screen.getByTestId("time-input"), "test")
         await user.click(screen.getByTestId("submit-button"))
         // expect(mockSubmitTimeString).lastCalledWith("test")
