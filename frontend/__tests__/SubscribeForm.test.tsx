0e2de88ac1c43334a6122231fcbe787fbd80b121
Jan Polivka <j.polivka5@gmail.com>
2023-04-23 19:16:51 +0000

    working

--- frontend/__tests__/SubscribeForm.test.tsx
+++ frontend/__tests__/SubscribeForm.test.tsx
@@ -1,18 +1,20 @@
 import '@testing-library/jest-dom'
-import { render, screen, waitFor } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { SubscribeForm } from '../src/SubscribeForm/SubscribeForm'
-import { submitTimeString } from '../src/SubscribeForm/submitTimeString'
 
 describe('SubscribeForm', () => {
     it('irenders the subscribe form', async () => {
         const user = userEvent.setup()
         const mockSubmitTimeString = jest.fn()
         render(<SubscribeForm submitTimeString={mockSubmitTimeString} />)
-        const timeInput = screen.getByTestId("time-input")
-        user.type(timeInput, "test")
-        user.click(screen.getByTestId("submit-button"))
-        // expect(screen.getByTestId("time-input")).toHaveValue("test")
+        const timeInput = screen.getByRole("textbox")
+        await user.type(timeInput, "YYYYYYYYYYYY")
+        // fireEvent.change(timeInput, { target: { value: "YYYYY" } })
+        // await user.click(screen.getByRole("textbox"))
+        // console.log(screen.getByRole("textbox"))
+        // expect(screen.getByRole("textbox")).toHaveValue("test")
+        // it user.click(screen.getByTestId("submit-button"))
         // expect(mockSubmitTimeString).lastCalledWith("test")
     })
 })
