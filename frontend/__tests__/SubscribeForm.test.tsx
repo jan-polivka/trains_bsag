a80455f30db15a47cec89c85105f5958809285bb
Jan Polivka <j.polivka5@gmail.com>
2023-04-23 16:23:16 +0000

    not working

--- frontend/__tests__/SubscribeForm.test.tsx
+++ frontend/__tests__/SubscribeForm.test.tsx
@@ -1,11 +1,17 @@
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import Home from '../pages/index'
 import { SubscribeForm } from '../src/SubscribeForm/SubscribeForm'
 import '@testing-library/jest-dom'
+import { submitTimeString } from '../src/SubscribeForm/submitTimeString'
+import userEvent from '@testing-library/user-event'
 
 describe('SubscribeForm', () => {
-    it('renders the subscribe form', () => {
-        // render(<SubscribeForm />)
-
+    it('irenders the subscribe form', async () => {
+        const user = userEvent.setup()
+        const mockSubmitTimeString = jest.fn()
+        render(<SubscribeForm submitTimeString={submitTimeString} />)
+        await user.type(screen.getByLabelText("time-input"), "test")
+        await user.click(screen.getByLabelText("submit-button"))
+        expect(mockSubmitTimeString).lastCalledWith("test")
     })
 })
---
+++ frontend/src/SubscribeForm/SubscribeForm.tsx
@@ -0,0 +1,25 @@
+import { Button, TextField } from "@mui/material"
+import Grid2 from "@mui/material/Unstable_Grid2"
+import { useState } from "react"
+
+export const SubscribeForm = (props: { submitTimeString: (timeString: string) => void }): JSX.Element => {
+
+    const [timeString, setTimeString] = useState("")
+
+
+
+
+    return (
+        <Grid2 container>
+            <Grid2 xs={"auto"}>
+                Enter time in the format HHMM:
+            </Grid2>
+            <Grid2 xs={"auto"}>
+                <TextField variant="outlined" onChange={(data) => setTimeString(data.target.value)} />
+            </Grid2>
+            <Grid2 xs={12}>
+                <Button onClick={() => props.submitTimeString(timeString)}>Submit</Button>
+            </Grid2>
+        </Grid2>
+    )
+}
