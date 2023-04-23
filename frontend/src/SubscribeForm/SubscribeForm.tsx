8aa98c2e748354abb7cfe7a05f3b837ef4bb8c43
Jan Polivka <j.polivka5@gmail.com>
2023-04-23 19:15:49 +0000

    working

--- frontend/src/SubscribeForm/SubscribeForm.tsx
+++ frontend/src/SubscribeForm/SubscribeForm.tsx
@@ -12,7 +12,7 @@ export const SubscribeForm = (props: { submitTimeString: (timeString: string) =>
                 Enter time in the format HHMM:
             </Grid2>
             <Grid2 xs={"auto"}>
-                <TextField data-testid="time-input" variant="outlined" value={"XXXXXXXXXXXXXXXx"} onInput={(data) => console.log(data)} onChange={(data) => setTimeString(data.target.value)} />
+                <TextField data-testid="time-input" variant="outlined" value={timeString} onInput={(data) => console.log(data)} onChange={(data) => setTimeString(data.target.value)} />
             </Grid2>
             <Grid2 xs={12}>
                 <Button data-testid="submit-button" onClick={() => props.submitTimeString(timeString)}>Submit</Button>
