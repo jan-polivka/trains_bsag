5e7cc80c9818e8f1d1c2ffd8f9aea1ba1cbc9f39
Jan Polivka <j.polivka5@gmail.com>
2023-04-26 17:25:07 +0000

    working

--- frontend/src/SubscribeForm/submitTimeString.ts
+++ frontend/src/SubscribeForm/submitTimeString.ts
@@ -1,4 +1,4 @@
-import fetch from "node-fetch"
+const fetch = require('node-fetch');
 
 export const submitTimeString = async (timeString: string): Promise<number> => {
     let response = await fetch("localhost:8080/submit_time_string", {
