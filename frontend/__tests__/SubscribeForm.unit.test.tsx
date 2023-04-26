0170a0961502a90b8a7befc4e13aefd3c8f31f5d
Jan Polivka <j.polivka5@gmail.com>
2023-04-26 18:20:50 +0000

    working

--- frontend/__tests__/SubscribeForm.unit.test.tsx
+++ frontend/__tests__/SubscribeForm.unit.test.tsx
@@ -1,10 +1,10 @@
 import '@testing-library/jest-dom'
-import { fireEvent, render, screen } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
+import { render, screen } from '@testing-library/react'
+import userEvent from "@testing-library/user-event";
 import { SubscribeForm } from '../src/SubscribeForm/SubscribeForm'
 
 describe('SubscribeForm', () => {
-    it('irenders the subscribe form', async () => {
+    it('renders the subscribe form', async () => {
         const user = userEvent.setup()
         const mockSubmitTimeString = jest.fn()
         render(<SubscribeForm submitTimeString={mockSubmitTimeString} />)
