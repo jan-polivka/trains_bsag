533f8444fbdf44a737c1cfd4a2aa4406c7a2d24a
Jan Polivka <j.polivka5@gmail.com>
2023-04-26 17:16:28 +0000

    working

--- frontend/src/SubscribeForm/submitTimeString.ts
+++ frontend/src/SubscribeForm/submitTimeString.ts
@@ -1,5 +1,5 @@
 export const submitTimeString = async (timeString: string): Promise<number> => {
-    let response = await fetch("localhost:8080/submit_time_string", {
+    let response = await window.fetch("localhost:8080/submit_time_string", {
         method: "POST",
         body: timeString
     })
