8ec9cad41ffdea8917fb65f48b3939ef15b95daf
Jan Polivka <j.polivka5@gmail.com>
2023-04-26 16:59:57 +0000

    working

--- frontend/src/SubscribeForm/submitTimeString.ts
+++ frontend/src/SubscribeForm/submitTimeString.ts
@@ -1,3 +1,7 @@
 export const submitTimeString = async (timeString: string): Promise<number> => {
+    fetch("localhost:8080", {
+        method: "POST",
+        body: timeString
+    })
     return 200
 }
