6900ed9c8d13e56a98381e09b24ff126307fd6f2
Jan Polivka <j.polivka5@gmail.com>
2023-04-26 17:30:53 +0000

    working

--- frontend/src/SubscribeForm/submitTimeString.ts
+++ frontend/src/SubscribeForm/submitTimeString.ts
@@ -3,8 +3,7 @@ const axios = require('axios');
 
 export const submitTimeString = async (timeString: string): Promise<number> => {
     let response = await axios.post("localhost:8080/submit_time_string", {
-        method: "POST",
-        body: timeString
+        timeString: timeString
     })
     return response.status
 }
