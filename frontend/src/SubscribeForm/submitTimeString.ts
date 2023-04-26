430950951a2eb3b570764868e11eb086c848f858
Jan Polivka <j.polivka5@gmail.com>
2023-04-26 17:38:21 +0000

    working

--- frontend/src/SubscribeForm/submitTimeString.ts
+++ frontend/src/SubscribeForm/submitTimeString.ts
@@ -1,6 +1,4 @@
 const axios = require('axios');
-const adapter = require('axios/lib/adapters/http')
-
 
 export const submitTimeString = async (timeString: string): Promise<number> => {
     let response = await axios.post("http://127.0.0.1:8080/submit_time_string", {
