c8c88590d36f24aa1da3096e6cbfda22167da032
Jan Polivka <j.polivka5@gmail.com>
2023-04-27 18:58:50 +0000

    test

--- backend/src/persistence/file_persistence.int.test.ts
+++ backend/src/persistence/file_persistence.int.test.ts
@@ -1,5 +1,13 @@
 import { expect, test } from "@jest/globals";
+import { loadConfig } from "../config_loader/config_loader";
+import { persistInFile } from "./file_persistence";
+import { fsync, readFileSync, rmSync } from "fs";
 
-test("simple test that shouldn't run", () => {
-    expect(1).toBe(1)
+test("simple test that shouldn't run", async () => {
+    const timeString = "1111"
+    const filePath = await loadConfig("config_test.yaml")
+    persistInFile(timeString)
+    let file = readFileSync(filePath['file'])
+    expect(file['timeString']).toBe(timeString)
+    rmSync(filePath)
 })
