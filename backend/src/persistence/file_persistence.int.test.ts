594112d85c0162ce8dec634be22ef2465b8e261f
Jan Polivka <j.polivka5@gmail.com>
2023-04-27 20:18:02 +0000

    working

--- backend/src/persistence/file_persistence.int.test.ts
+++ backend/src/persistence/file_persistence.int.test.ts
@@ -7,7 +7,7 @@ test("file persistence integration test", async () => {
     const timeString = "1111"
     const parsedYaml = await loadConfig("config_test.yaml")
     const filePath = parsedYaml['file']
-    persistInFile(timeString, filePath)
+    await persistInFile(timeString, filePath)
     let file = JSON.parse(readFileSync(filePath, 'utf-8'))
     expect(file['timeString']).toBe(timeString)
     rmSync(filePath)
--- backend/src/persistence/file_persistence.ts
+++ backend/src/persistence/file_persistence.ts
@@ -1,12 +1,12 @@
 import { readFileSync, writeFileSync } from "fs"
-import { writeFile } from "fs/promises"
+import { readFile, writeFile } from "fs/promises"
 
 export const persistInFile = async (timeString: string, filepath: string) => {
     const jsonTimeString = JSON.stringify({ timeString: timeString })
     await writeFile(filepath, jsonTimeString)
 }
 
-export const retrieveFromFile = (filepath: string): string => {
-    readFileSync
-    return ""
+export const retrieveFromFile = async (filepath: string): Promise<string> => {
+    const timeStringFile = await readFile(filepath)
+    return timeStringFile['timeString']
 }
