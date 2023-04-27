1cfa2e7610df39cf3eb08f65a6fa11880592fe58
Jan Polivka <j.polivka5@gmail.com>
2023-04-27 20:26:08 +0000

    working

--- backend/src/persistence/file_persistence.ts
+++ backend/src/persistence/file_persistence.ts
@@ -7,8 +7,9 @@ export const persistInFile = async (timeString: string, filepath: string) => {
 }
 
 export const retrieveFromFile = async (filepath: string): Promise<string> => {
-    const timeStringFile = await readFile(filepath, "utf-8").catch(err => {
-        return err
-    })
+    const timeStringFile = await readFile(filepath, "utf-8")
+        .catch(err => {
+            return err
+        })
     return JSON.parse(timeStringFile)['timeString']
 }
