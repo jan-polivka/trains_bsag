f2c75d9ef847fab2b6812876927439c0d8b786b9
Jan Polivka <j.polivka5@gmail.com>
2023-04-27 19:22:47 +0000

    working

--- backend/src/persistence/file_persistence.int.test.ts
+++ backend/src/persistence/file_persistence.int.test.ts
@@ -1,13 +1,13 @@
 import { expect, test } from "@jest/globals";
 import { loadConfig } from "../config_loader/config_loader";
 import { persistInFile } from "./file_persistence";
-import { fsync, readFileSync, rmSync } from "fs";
+import { readFileSync, rmSync } from "fs";
 
 test("file persistence integration test", async () => {
     const timeString = "1111"
     const parsedYaml = await loadConfig("config_test.yaml")
     const filePath = parsedYaml['file']
-    await persistInFile(timeString, filePath)
+    persistInFile(timeString, filePath)
     let file = JSON.parse(readFileSync(filePath, 'utf-8'))
     expect(file['timeString']).toBe(timeString)
     rmSync(filePath)
--- backend/src/persistence/file_persistence.ts
+++ backend/src/persistence/file_persistence.ts
@@ -1,6 +1,6 @@
 import { writeFileSync } from "fs"
 
-export const persistInFile = async (timeString: string, filepath: string) => {
+export const persistInFile = (timeString: string, filepath: string) => {
     const jsonTimeString = JSON.stringify({ timeString: timeString })
     writeFileSync(filepath, jsonTimeString)
 }
