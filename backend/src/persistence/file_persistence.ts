7a1397ea26c6d39b6f917ddbb36384c41be530ce
Jan Polivka <j.polivka5@gmail.com>
2023-04-27 19:17:46 +0000

    working

--- backend/src/persistence/file_persistence.ts
+++ backend/src/persistence/file_persistence.ts
@@ -2,6 +2,6 @@ import { writeFile } from "fs/promises"
 
 export const persistInFile = async (timeString: string, filepath: string) => {
     const jsonTimeString = JSON.stringify({ timeString: timeString })
-    console.log(jsonTimeString)
+    // console.log(jsonTimeString)
     writeFile(filepath, jsonTimeString, 'utf-8')
 }
