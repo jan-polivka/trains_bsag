fa06b22effb87a17cc63ad04cdbb75b6c6f45a76
Jan Polivka <j.polivka5@gmail.com>
2023-04-27 19:09:15 +0000

    working

--- backend/src/persistence/file_persistence.ts
+++ backend/src/persistence/file_persistence.ts
@@ -1,5 +1,6 @@
 import { writeFile } from "fs/promises"
 
 export const persistInFile = async (timeString: string, filepath: string) => {
+    console.log(filepath)
     writeFile(filepath, timeString)
 }
