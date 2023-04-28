1ae023c1e1de6ad2cbb00db5ecf5cff0850227f3
Jan Polivka <j.polivka5@gmail.com>
2023-04-28 17:54:21 +0000

    working

--- frontend/src/RetrieveForm/RetrieveForm.tsx
+++ frontend/src/RetrieveForm/RetrieveForm.tsx
@@ -1,3 +1,9 @@
+import { Button } from "@mui/material"
+
 export const RetrieveForm = (props: { retrieveTimeString: () => string }): JSX.Element => {
-    return <></>
+    return (
+        <>
+            <Button aria-label="retrieve-time-string">Retrieve time string</Button>
+        </>
+    )
 }
