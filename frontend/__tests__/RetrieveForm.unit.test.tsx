a3e8f2cd76771c411825412d5609f14659818d4d
Jan Polivka <j.polivka5@gmail.com>
2023-04-28 17:39:52 +0000

    working

--- frontend/__tests__/RetrieveForm.unit.test.tsx
+++ frontend/__tests__/RetrieveForm.unit.test.tsx
@@ -3,7 +3,9 @@ import { RetrieveForm } from "../src/RetrieveForm/RetrieveForm"
 
 describe("RetrieveForm", () => {
     it("renders the retrieve form", () => {
-        // render(<RetrieveForm />)
-
+        const mockRetrieveTimeString = jest.fn(() => "1111")
+        render(<RetrieveForm retrieveTimeString={mockRetrieveTimeString} />)
+        //verify that mock has been called
+        //verify that there is a label and it carries the timeString
     })
 })
