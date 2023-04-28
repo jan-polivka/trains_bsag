0f208e28363a55deee6e650290731e4f7b9831b6
Jan Polivka <j.polivka5@gmail.com>
2023-04-28 17:31:06 +0000

    untracked

---
+++ frontend/__tests__/RetrieveForm.unit.test.tsx
@@ -0,0 +1,9 @@
+import { render } from "@testing-library/react"
+import { RetrieveForm } from "../src/RetrieveForm/RetrieveForm"
+
+describe("RetrieveForm", () => {
+    it("renders the retrieve form", () => {
+        render(<RetrieveForm />)
+
+    })
+})
---
+++ frontend/src/RetrieveForm/RetrieveForm.tsx
@@ -0,0 +1,3 @@
+export const RetrieveForm = (): JSX.Element => {
+    return <></>
+}
