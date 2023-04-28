1f7277c0887221f016bbbaaa46efeb9ce2df2bc9
Jan Polivka <j.polivka5@gmail.com>
2023-04-28 19:29:28 +0000

    untracked

---
+++ frontend/__tests__/retrieveTimeString.int.test.ts
@@ -0,0 +1,11 @@
+import { retrieveTimeString } from "../src/RetrieveForm/retrieveTimeString"
+import { submitTimeString } from "../src/SubscribeForm/submitTimeString"
+
+describe("integration tests for retrieveTimeString", () => {
+    it("there is a string on the server", async () => {
+        // let response = await submitTimeString("1010")
+        const retrieved = await retrieveTimeString()
+        expect(retrieved).toBe("")
+
+    })
+})
