a30ba8a1e55f2968381a11bdab95c2de2577b9e0
Jan Polivka <j.polivka5@gmail.com>
2023-05-01 16:52:40 +0000

    test fixing

--- frontend/__tests__/RetrieveForm.unit.test.tsx
+++ frontend/__tests__/RetrieveForm.unit.test.tsx
@@ -1,17 +1,17 @@
-import { render, screen } from "@testing-library/react"
-import { RetrieveForm } from "../src/RetrieveForm/RetrieveForm"
-import { userAgent } from "next/server"
-import userEvent from "@testing-library/user-event"
+import { render, screen } from "@testing-library/react";
+import { RetrieveForm } from "../src/RetrieveForm/RetrieveForm";
+import { userAgent } from "next/server";
+import userEvent from "@testing-library/user-event";
 
 describe("RetrieveForm", () => {
-    it("renders the retrieve form", async () => {
-        const user = userEvent.setup()
-        const mockRetrieveTimeString = jest.fn(() => "1111")
-        render(<RetrieveForm retrieveTimeString={mockRetrieveTimeString} />)
-        const retrieveButton = screen.getByLabelText("retrieve-time-string")
-        await user.click(retrieveButton)
-        const retrievedTimeString = screen.getByLabelText("retrieved-time-string")
-        expect(retrievedTimeString.textContent).toBe("1111")
-        expect(mockRetrieveTimeString).toBeCalled()
-    })
-})
+  it("renders the retrieve form", async () => {
+    const user = userEvent.setup();
+    const mockRetrieveTimeString = jest.fn(async () => "1111");
+    render(<RetrieveForm retrieveTimeString={mockRetrieveTimeString} />);
+    const retrieveButton = screen.getByLabelText("retrieve-time-string");
+    await user.click(retrieveButton);
+    const retrievedTimeString = screen.getByLabelText("retrieved-time-string");
+    expect(retrievedTimeString.textContent).toBe("1111");
+    expect(mockRetrieveTimeString).toBeCalled();
+  });
+});
--- frontend/__tests__/submitTimeString.int.test.ts
+++ frontend/__tests__/submitTimeString.int.test.ts
@@ -3,12 +3,12 @@ import { submitTimeString } from "../src/SubscribeForm/submitTimeString"
 
 describe("integration tests", () => {
     it("submission successful", async () => {
-        let response = await submitTimeString("1010")
+        let response = await submitTimeString("10", "01")
         assert.equal(response, "OK")
     })
 
     it("submission unsuccessful", async () => {
-        let response = await submitTimeString("1a1a")
+        let response = await submitTimeString("1a", "a1")
         assert.equal(response, "NOT OK")
     })
 })
