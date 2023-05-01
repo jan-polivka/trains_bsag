4e58b10a532a1a31833f06bbc4174d3b19f37f0e
Jan Polivka <j.polivka5@gmail.com>
2023-05-01 06:18:25 +0000

    working

--- frontend/src/RetrieveForm/RetrieveForm.tsx
+++ frontend/src/RetrieveForm/RetrieveForm.tsx
@@ -1,15 +1,23 @@
-import { Button } from "@mui/material"
-import { useState } from "react"
+import { Button } from "@mui/material";
+import { useState } from "react";
 
-export const RetrieveForm = (props: { retrieveTimeString: () => string }): JSX.Element => {
+export const RetrieveForm = (props: {
+  retrieveTimeString: () => Promise<string>;
+}): JSX.Element => {
+  const [timeString, setTimeString] = useState("");
 
-    const [timeString, setTimeString] = useState("")
-
-    return (
-        <>
-            <label aria-label="retrieved-time-string">{timeString}</label>
-            <br />
-            <Button aria-label="retrieve-time-string" onClick={() => { setTimeString(props.retrieveTimeString()) }}>Retrieve time string</Button>
-        </>
-    )
-}
+  return (
+    <>
+      <label aria-label="retrieved-time-string">{timeString}</label>
+      <br />
+      <Button
+        aria-label="retrieve-time-string"
+        onClick={async () => {
+          setTimeString(await props.retrieveTimeString());
+        }}
+      >
+        Retrieve time string
+      </Button>
+    </>
+  );
+};
