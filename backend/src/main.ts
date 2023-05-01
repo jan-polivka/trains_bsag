92a172319fef72e05c195887bbbd78dad526f882
Jan Polivka <j.polivka5@gmail.com>
2023-05-01 08:18:16 +0000

    working

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -73,7 +73,7 @@ fastify.get('/retrieve_time_string', async (req, res) => {
 fastify.listen({ port: 8080 }, () => {
     console.log("doing the listen")
     const scheduledJobs = new Array()
-    console.log(Date.now())
+    console.log(Date.now().toLocaleString())
 })
 
 const job = schedule.scheduleJob(`17 10 * * *`, function () {
