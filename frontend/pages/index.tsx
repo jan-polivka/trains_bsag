b471e74e48f5482485e29cd241969b0a9b6eb688
Jan Polivka <j.polivka5@gmail.com>
2023-05-01 06:17:29 +0000

    working

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,22 +1,20 @@
-import Grid2 from "@mui/material/Unstable_Grid2"
-import { SubscribeForm } from "../src/SubscribeForm/SubscribeForm"
-import { submitTimeString } from "../src/SubscribeForm/submitTimeString"
-import { RetrieveForm } from "../src/RetrieveForm/RetrieveForm"
+import Grid2 from "@mui/material/Unstable_Grid2";
+import { SubscribeForm } from "../src/SubscribeForm/SubscribeForm";
+import { submitTimeString } from "../src/SubscribeForm/submitTimeString";
+import { RetrieveForm } from "../src/RetrieveForm/RetrieveForm";
 
 const homePage = (): any => {
-    return (
-        <Grid2 container>
-            <Grid2 xs={12}>
-                Hello world!
-            </Grid2>
-            <Grid2 xs={12}>() => "1234"
-                <RetrieveForm retrieveTimeString={retrieveTimeString} />
-            </Grid2>
-            <Grid2 xs={"auto"}>
-                <SubscribeForm submitTimeString={submitTimeString} />
-            </Grid2>
-        </Grid2>
-    )
-}
+  return (
+    <Grid2 container>
+      <Grid2 xs={12}>Hello world!</Grid2>
+      <Grid2 xs={12}>
+        <RetrieveForm retrieveTimeString={() => "1234"} />
+      </Grid2>
+      <Grid2 xs={"auto"}>
+        <SubscribeForm submitTimeString={submitTimeString} />
+      </Grid2>
+    </Grid2>
+  );
+};
 
-export default homePage
+export default homePage;
