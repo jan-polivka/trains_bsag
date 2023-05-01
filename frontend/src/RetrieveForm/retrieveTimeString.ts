86f915821b9c5921aa94767e9b3f0b74e378c38e
Jan Polivka <j.polivka5@gmail.com>
2023-05-01 06:48:01 +0000

    working

--- frontend/src/RetrieveForm/retrieveTimeString.ts
+++ frontend/src/RetrieveForm/retrieveTimeString.ts
@@ -3,5 +3,5 @@ const axios = require('axios');
 export const retrieveTimeString = async (): Promise<string> => {
     const timeString = await axios.get("http://127.0.0.1:8080/retrieve_time_string")
     console.log(timeString.data)
-    return ""
+    return timeString.data
 }
