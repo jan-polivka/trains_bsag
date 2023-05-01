a20e92ca03495de327c7dbe5b92addf592910425
Jan Polivka <j.polivka5@gmail.com>
2023-05-01 06:15:14 +0000

    working

--- frontend/src/SubscribeForm/SubscribeForm.tsx
+++ frontend/src/SubscribeForm/SubscribeForm.tsx
@@ -1,22 +1,31 @@
-import { Button, TextField } from "@mui/material"
-import Grid2 from "@mui/material/Unstable_Grid2"
-import { useState } from "react"
+import { Button, TextField } from "@mui/material";
+import Grid2 from "@mui/material/Unstable_Grid2";
+import { useState } from "react";
 
-export const SubscribeForm = (props: { submitTimeString: (timeString: string) => void }): JSX.Element => {
+export const SubscribeForm = (props: {
+  submitTimeString: (timeString: string) => void;
+}): JSX.Element => {
+  const [timeString, setTimeString] = useState("");
 
-    const [timeString, setTimeString] = useState("")
-
-    return (
-        <Grid2 container>
-            <Grid2 xs={"auto"}>
-                Enter time in the format HHMM:
-            </Grid2>
-            <Grid2 xs={"auto"}>
-                <TextField inputProps={{ 'aria-label': 'time-input' }} variant="outlined" value={timeString} onChange={(data) => setTimeString(data.target.value)} />
-            </Grid2>
-            <Grid2 xs={12}>
-                <Button aria-label="submit-time-input" onClick={() => props.submitTimeString(timeString)}>Submit</Button>
-            </Grid2>
-        </Grid2>
-    )
-}
+  return (
+    <Grid2 container>
+      <Grid2 xs={"auto"}>Enter time in the format HHMM:</Grid2>
+      <Grid2 xs={"auto"}>
+        <TextField
+          inputProps={{ "aria-label": "time-input" }}
+          variant="outlined"
+          value={timeString}
+          onChange={(data) => setTimeString(data.target.value)}
+        />
+      </Grid2>
+      <Grid2 xs={12}>
+        <Button
+          aria-label="submit-time-input"
+          onClick={() => props.submitTimeString(timeString)}
+        >
+          Submit
+        </Button>
+      </Grid2>
+    </Grid2>
+  );
+};
