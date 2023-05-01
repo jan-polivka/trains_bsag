9735ad5d4bbf8101af19ab076de02ffa6634daaa
Jan Polivka <j.polivka5@gmail.com>
2023-05-01 06:18:50 +0000

    working

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -2,13 +2,14 @@ import Grid2 from "@mui/material/Unstable_Grid2";
 import { SubscribeForm } from "../src/SubscribeForm/SubscribeForm";
 import { submitTimeString } from "../src/SubscribeForm/submitTimeString";
 import { RetrieveForm } from "../src/RetrieveForm/RetrieveForm";
+import { retrieveTimeString } from "../src/RetrieveForm/retrieveTimeString";
 
 const homePage = (): any => {
   return (
     <Grid2 container>
       <Grid2 xs={12}>Hello world!</Grid2>
       <Grid2 xs={12}>
-        <RetrieveForm retrieveTimeString={() => "1234"} />
+        <RetrieveForm retrieveTimeString={retrieveTimeString} />
       </Grid2>
       <Grid2 xs={"auto"}>
         <SubscribeForm submitTimeString={submitTimeString} />
