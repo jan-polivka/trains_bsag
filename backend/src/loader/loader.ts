77b8d331c82ea9e12af65f3a3f677d977f716d58
Jan Polivka <j.polivka5@gmail.com>
2023-05-05 18:42:34 +0000

    working

--- backend/src/loader/loader.ts
+++ backend/src/loader/loader.ts
@@ -44,6 +44,6 @@ export const connectionGetBremen = async (): Promise<String> => {
     let resp = await fetch(urlBremen, optionsBremen)
     // console.log(await resp.json())
     const json = await resp.json()
-    console.log(json['plan']['itineraries'][0])
+    console.log(json['plan']['itineraries'][0]['legs'])
     return ""
 }
