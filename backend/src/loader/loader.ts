1bd59e7da3c5abb10b7b3d2849f2f11e9b8507f5
Jan Polivka <j.polivka5@gmail.com>
2023-05-05 18:40:30 +0000

    working

--- backend/src/loader/loader.ts
+++ backend/src/loader/loader.ts
@@ -32,7 +32,7 @@ export const connectionGetBremen = async (): Promise<String> => {
     const start = '53.169621,8.627897'
     const destination = '53.103839,8.786177'
     // const urlBremen = `http://gtfsr.vbn.de/api/routers/connect/plan?arriveBy=false&date=05-10-2023&fromPlace=53.08287,8.81334&toPlace=53.05270,8.78617&time=13:00:00&mode=WALK,TRANSIT&maxWalkDistance=300`
-    const urlBremen = `http://gtfsr.vbn.de/api/routers/connect/plan?arriveBy=false&date=05-10-2023&fromPlace=${start}&toPlace=${destination}&time=13:00:00&mode=TRANSIT`
+    const urlBremen = `http://gtfsr.vbn.de/api/routers/connect/plan?arriveBy=false&date=05-10-2023&fromPlace=${start}&toPlace=${destination}&time=13:00:00&mode=WALK,TRANSIT&maxWalkDistance=300`
 
     console.log(apiKey)
     const optionsBremen = {
