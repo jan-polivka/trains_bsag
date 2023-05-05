c1febdd2ee419fadfef7972dfcafafa3dc989174
Jan Polivka <j.polivka5@gmail.com>
2023-05-05 18:33:22 +0000

    working

--- backend/src/loader/loader.ts
+++ backend/src/loader/loader.ts
@@ -28,7 +28,6 @@ export async function connectionGet(): Promise<Connections> {
 
 export const connectionGetBremen = async (): Promise<String> => {
     const config = await loadConfig('config_default.yaml')
-    console.log(config)
     const apiKey = config['api_key']
     const start = '53.169621000000,8.627897000000'
     const destination = '53.103839000000,8.786177000000'
